d7c184d22da0226fb6114369cb8ea1c6800afda1
johan3008 <95117373+johan3008@users.noreply.github.com>
2026-07-12 00:17:12 +0700

    Fix template literals in MotionGenView.tsx

--- MotionGenView.tsx
+++ MotionGenView.tsx
@@ -255,7 +255,7 @@ export const MotionComposition = () => {
                 
                 <div id="motion-gen-player" className="w-full h-full max-h-[80vh] flex items-center justify-center relative">
                     {/* Mengatur rasio kontainer */}
-                    <div style={{ aspectRatio: \`${width}/${height}\`, maxHeight: '100%', maxWidth: '100%' }} className="bg-black rounded-lg border border-[#333] shadow-2xl overflow-hidden relative">
+                    <div style={{ aspectRatio: `\${width}/\${height}`, maxHeight: '100%', maxWidth: '100%' }} className="bg-black rounded-lg border border-[#333] shadow-2xl overflow-hidden relative">
                         <LiveRemotionRunner code={code} fps={fps} durationInFrames={durationInFrames} width={width} height={height} />
                     </div>
                 </div>
@@ -277,7 +277,7 @@ export const MotionComposition = () => {
                             <div className="h-3 w-full bg-[#222] rounded-full overflow-hidden">
                                 <div 
                                     className="h-full bg-gradient-to-r from-blue-500 to-indigo-500 transition-all duration-300 ease-out"
-                                    style={{ width: \`${renderProgress.progressPercentage}%\` }}
+                                    style={{ width: `\${renderProgress.progressPercentage}%` }}
                                 ></div>
                             </div>
                             
